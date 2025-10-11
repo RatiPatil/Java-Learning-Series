2a924a84aa9885c1810c2a4648ef9cd5784fbdbd
Ratikant Patil <patilratikant8@gmail.com>
2025-10-11 14:42:54 +0530

    Comments

---
+++ Programs/Comments.java
@@ -0,0 +1,10 @@
+public class Comments {
+  public static void main(String[] args) {
+    /*
+     * The code below will print the words Hello World
+     * to the screen, and it is amazing
+     */
+    System.out.println("Hello World");
+
+  }
+}
