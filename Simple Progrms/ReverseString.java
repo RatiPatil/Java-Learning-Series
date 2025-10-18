a82307dda453604dab4b577af4a205d6f425e378
Ratikant Patil <patilratikant8@gmail.com>
2025-10-18 09:10:57 +0530

    ReverseString

---
+++ Simple Progrms/ReverseString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ReverseString {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a string: ");
+    String str = sc.nextLine(); // take input
+
+    String reversed = ""; // to store reversed string
+
+    // reverse using a loop
+    for (int i = str.length() - 1; i >= 0; i--) {
+      reversed = reversed + str.charAt(i);
+    }
+
+    System.out.println("Reversed string: " + reversed);
+  }
+}
