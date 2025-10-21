a8d44106a556e96b050ae64f06d45b30811f67c7
RatiPatil <patilratikant8@gmail.com>
2025-10-21 21:06:23 +0530

    Small Changes

--- Simple Progrms/CountDigits.java
+++ Simple Progrms/CountDigits.java
@@ -5,9 +5,11 @@ public class CountDigits {
 
     while (num != 0) {
       num = num / 10; // remove last digit
-      count++;        // increase count
+      count++; // increase count
     }
 
     System.out.println("Total digits: " + count);
   }
 }
+
+// test
