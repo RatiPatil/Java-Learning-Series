0613a756757754175f13e5a8068fd936569cf9e4
Rati Patil <patilratikant8@gmail.com>
2025-11-29 15:24:58 +0530

    ReverseNumberCenter

---
+++ Patterns/ReverseNumberCenter.java
@@ -0,0 +1,18 @@
+public class ReverseNumberCenter {
+    public static void main(String[] args) {
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s < i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j <= n; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
